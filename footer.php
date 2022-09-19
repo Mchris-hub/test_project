032c88f832767a685c470183fba9ea8a2bba7b74
christian <christianmambu2@gmail.com>
2022-09-19 16:42:36 +0200

    header and footer done

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php wp_footer(); ?>
+<footer>
 <div class="container">
 <div id="footer" class="row">
      <div class="col-sm-3">
@@ -29,10 +30,12 @@
        </ul>
      </div>
      <div class="col-sm-3">
-     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19267.291531129707!2d4.795549282790971!3d52.869004156095095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4eb2d3b5478b%3A0x397b061362abc575!2sKaas+%26+Jacob!5e0!3m2!1sen!2sza!4v1556018881315!5m2!1sen!2sza" width="400" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d19267.291531129707!2d4.795549282790971!3d52.869004156095095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf4eb2d3b5478b%3A0x397b061362abc575!2sKaas+%26+Jacob!5e0!3m2!1sen!2sza!4v1556018881315!5m2!1sen!2sza" width="200" height="300" frameborder="0" style="border:0" allowfullscreen=""></iframe>
      </div>
 </div>
 </div>
+</footer>
+
 
 </body>
 </html>
