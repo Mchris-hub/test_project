8081d1f9ce78000d36865aa650936861b9c0af58
christian <christianmambu2@gmail.com>
2022-09-21 12:07:32 +0200

    advanced custom fields

--- front-page.php
+++ front-page.php
@@ -5,7 +5,28 @@
         
               <h1><?php  the_title( );  ?></h1>
               
-              <?php get_template_part('includes/section','content'); ?>
+              <?php 
+              get_template_part('includes/section','content'); 
+              
+              ?>
+              
+             <br>
+             <div class="container">
+                 
+                   <h1><?php the_field("phone_number"); ?></h1>
+                   <h1><?php the_field("email"); ?></h1>
+
+             </div>
+             <br>
+             <div class="container">
+             <?php 
+             $image = get_field('picture');
+             $picture = $image['sizes']['large'];
+
+             
+             ?>
+             <img src="<?php  echo $picture ?>"class="img-field">
+             </div>
              
             
 </div>
